08d285f9393806263ac65567679ec8826347c73d
khyojun <nandong5923@naver.com>
2022-11-27 12:14:12 +0900

    feat(baseball): 사용자 입력을 받기 전 출력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,19 +1,29 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lotto.Lotto;
 import lotto.domain.Validate;
 
 public class InputView {
 
     Validate validate = new Validate();
     OutputView outputView = new OutputView();
-    public void payMoney() {
+    public int payMoney() {
         try {
+            outputView.printPayMoney();
             String userMoney = Console.readLine();
             validate.validateUserMoney(userMoney);
         }catch(IllegalArgumentException e){
             outputView.printError(e.getMessage());
-            payMoney();
+            return payMoney();
         }
+        return validate.getConvertedUserMoney();
     }
+
+
+
+
 }
