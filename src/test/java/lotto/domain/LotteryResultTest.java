1e982e944fbab3783e748d32adb2778004cae203
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 14:36:26 +0900

    test(LotteryResultTest): 수익률 계산 기능 테스트 추가

--- src/test/java/lotto/domain/LotteryResultTest.java
+++ src/test/java/lotto/domain/LotteryResultTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.constant.LotteryRank;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -8,26 +9,39 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LotteryResultTest {
+    private static LotteryResult lotteryResult;
+
+    @BeforeEach
+    void beforeEach() {
+        lotteryResult = new LotteryResult("id");
+        lotteryResult.add(LotteryRank.FIFTH);
+        lotteryResult.add(LotteryRank.FOURTH);
+        lotteryResult.add(LotteryRank.LOSE_WITH_ONE_MATCH);
+        lotteryResult.add(LotteryRank.LOSE_WITH_ONE_MATCH);
+        lotteryResult.add(LotteryRank.LOSE_WITH_NO_MATCH);
+        lotteryResult.add(LotteryRank.LOSE_WITH_TWO_MATCHES);
+        lotteryResult.add(LotteryRank.LOSE_WITH_NO_MATCH);
+    }
+
     @Test
     void addResultTest() {
-        LotteryResult lotteryResult = new LotteryResult("id");
-        lotteryResult.add(LotteryRank.FIFTH);
-        lotteryResult.add(LotteryRank.FIRST);
-        lotteryResult.add(LotteryRank.SECOND);
-        lotteryResult.add(LotteryRank.LOSE_WITH_ONE_MATCH);
-        lotteryResult.add(LotteryRank.SECOND);
-
         Map<LotteryRank, Integer> results = Map.of(
-                LotteryRank.FIRST, 1,
-                LotteryRank.SECOND, 2,
+                LotteryRank.FIRST, 0,
+                LotteryRank.SECOND, 0,
                 LotteryRank.THIRD, 0,
-                LotteryRank.FOURTH, 0,
+                LotteryRank.FOURTH, 1,
                 LotteryRank.FIFTH, 1,
-                LotteryRank.LOSE_WITH_TWO_MATCHES, 0,
-                LotteryRank.LOSE_WITH_ONE_MATCH, 1,
-                LotteryRank.LOSE_WITH_NO_MATCH, 0
+                LotteryRank.LOSE_WITH_TWO_MATCHES, 1,
+                LotteryRank.LOSE_WITH_ONE_MATCH, 2,
+                LotteryRank.LOSE_WITH_NO_MATCH, 2
         );
 
         assertThat(lotteryResult.getResults()).isEqualTo(results);
+    }
+
+    @Test
+    void calculateProfitRateTest() {
+        double profitRate = lotteryResult.calculateProfitRate();
+        assertThat(String.format("%.1f", profitRate)).isEqualTo("785.7");
     }
 }
