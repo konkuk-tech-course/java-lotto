0b04dd52dec04f56d25a9374ec77f4092494ccc4
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 12:09:58 +0900

    feat(DomainErrorMessage): 당첨번호 유효성 관련 에러메세지 추가

--- src/main/java/lotto/domain/constant/DomainErrorMessage.java
+++ src/main/java/lotto/domain/constant/DomainErrorMessage.java
@@ -6,7 +6,8 @@ public enum DomainErrorMessage {
             + "까지의 숫자만 허용됩니다."),
     INVALID_RANGE_BONUS_NUMBER("보너스 번호는 " + LottoProperties.MIN_NUMBER.get() + "부터 " + LottoProperties.MAX_NUMBER.get()
             + "까지의 숫자만 허용됩니다."),
-    DUPLICATED_NUMBERS("중복된 숫자가 없도록 입력해주세요");
+    DUPLICATED_NUMBERS("중복된 숫자가 없도록 입력해주세요"),
+    DUPLICATE_NUMBERS_WITH_BONUS("보너스 번호는 기존 6자리 숫자와 중복되지 않도록 입력해주세요.");
 
     private static final String prefix = "[ERROR] ";
     private final String message;
