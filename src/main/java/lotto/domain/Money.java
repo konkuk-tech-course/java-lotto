b307c46a39027e466c2f8164b81eb104340acfaa
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 13:33:25 +0900

    feat(Money): 클래스 구현 및 유효성 검증 메서드 구현

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import lotto.domain.constant.DomainErrorMessage;
+
+public class Money {
+    private int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        if (DomainValidator.isMoneyZero(money)) {
+            throw new IllegalArgumentException(DomainErrorMessage.NO_MONEY.get());
+        }
+
+        if (!DomainValidator.isMultiplyOfCorrectUnit(money)) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_UNIT.get());
+        }
+    }
+}
