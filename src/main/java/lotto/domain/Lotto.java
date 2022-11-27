b08e2ed99a3797ba7ebeb8a3079563755992d137
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 11:31:54 +0900

    feat(Lotto): 유효성 검증 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.domain.constant.DomainErrorMessage;
+import lotto.domain.constant.LottoProperties;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,10 +14,34 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (isIncorrectSize(numbers)) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_SIZE.get());
+        }
+
+        if (hasInvalidRangeNumber(numbers)) {
+            throw new IllegalArgumentException(DomainErrorMessage.INVALID_RANGE.get());
+        }
+
+        if (hasSameNumber(numbers)) {
+            throw new IllegalArgumentException(DomainErrorMessage.DUPLICATED_NUMBERS.get());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isIncorrectSize(List<Integer> numbers) {
+        return numbers.size() != LottoProperties.SIZE.get();
+
+    }
+
+    private boolean hasInvalidRangeNumber(List<Integer> numbers) {
+        return numbers.stream().anyMatch(this::isInvalidRange);
+    }
+
+    private boolean isInvalidRange(int number) {
+        return number < LottoProperties.MIN_NUMBER.get()
+                && number > LottoProperties.MAX_NUMBER.get();
+    }
+
+    private boolean hasSameNumber(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != LottoProperties.SIZE.get();
+    }
 }
