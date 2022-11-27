2ef1e0946e867bf3f71c28dca7bc7d28f6ff09a4
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:05:57 +0900

    feat(InputView): 구입 금액 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.view.constant.InputPhrase;
+
 public class InputView {
     private static InputView instance = new InputView();
 
@@ -8,5 +11,12 @@ public class InputView {
 
     public static InputView getInstance() {
         return instance;
+    }
+
+    public int readMoney() {
+        System.out.println(InputPhrase.MONEY_INPUT.get());
+        String moneyInput = Console.readLine();
+        ViewValidator.validateNumberInput(moneyInput);
+        return Integer.parseInt(moneyInput);
     }
 }
