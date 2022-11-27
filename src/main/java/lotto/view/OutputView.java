eea211516ecd83f45ba6b97a5a80753c94276e9a
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:28:58 +0900

    feat(OutputView): 구매한 로또 티켓 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,15 @@
 package lotto.view;
 
+import lotto.view.constant.OutputPhrase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+    private static final String DELIMITER = ", ";
+    private static final String TICKET_PREFIX = "[";
+    private static final String TICKET_SUFFIX = "]";
+
     private static OutputView instance = new OutputView();
 
     private OutputView() {
@@ -10,7 +19,29 @@ public class OutputView {
         return instance;
     }
 
+    public void printPurchasedTickets(List<List<Integer>> tickets) {
+        System.out.printf(OutputPhrase.PURCHASE_COUNT.get(), tickets.size());
+        insertLineBreak();
+        tickets.stream()
+                .map(this::styleTicket)
+                .forEach(System.out::println);
+    }
+
+    private String styleTicket(List<Integer> ticket) {
+        return ticket.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(
+                DELIMITER,
+                TICKET_PREFIX,
+                TICKET_SUFFIX
+        ));
+    }
+
     public void printErrorMessage(String message) {
         System.out.println(message);
+    }
+
+    public void insertLineBreak() {
+        System.out.println();
     }
 }
