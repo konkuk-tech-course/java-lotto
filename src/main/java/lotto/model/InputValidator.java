9b40cb185948aaa5634a764f2e184ea15840a63e
Life is a ninanino <tvxqinak@naver.com>
2022-11-27 12:56:41 +0900

    feat : 입력받은 로또번호, 보너스 번호 유효성 검사, 메세지 추가

--- src/main/java/lotto/model/InputValidator.java
+++ src/main/java/lotto/model/InputValidator.java
@@ -2,6 +2,11 @@ package lotto.model;// @ author ninaaano
 
 import lotto.model.constant.InputValidMessage;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
 import static lotto.model.constant.CommonCostants.IS_NUMBER;
 import static lotto.model.constant.CommonCostants.MIN_LOTTO_PRICE;
 
@@ -10,6 +15,63 @@ public class InputValidator {
     public InputValidator() {
     }
 
+    /**
+     * 입력받은 로또 번호 유효성 검사
+     */
+    public static boolean isValidNumber(String inputNumber){
+        try {
+            isCheckNumber(inputNumber);
+            int number = Integer.parseInt(inputNumber);
+            validateLottoNumber(number);
+            return true;
+        }catch (IllegalArgumentException e) {
+            System.out.println(InputValidMessage.INPUT_NUMBER_ERROR);
+            return false;
+        }
+    }
+
+    public static void validateLottoNumber(int number) {
+        if(!checkRange(number)) {
+            System.out.println("로또 번호는 1 ~ 45 사이의 번호여야 합니다.");
+            throw new IllegalArgumentException(InputValidMessage.AMOUNT_RANGE.get());
+        }
+    }
+    public static boolean checkRange(int number){
+        return number >= 1 && number <= 45;
+    }
+
+    public static void isValidBonusNumber(Lotto lottoNumber, int bonusNumber){
+        if(lottoNumber.contains(bonusNumber)){
+            throw new IllegalArgumentException(InputValidMessage.REDUPLICATION.get());
+        }
+
+    }
+
+    /**
+     * 로또 번호 검사
+     * @param numbers
+     */
+    public static void validLottoNumberSize(List<Integer> numbers) {
+        if(numbers.size() != 6){
+            throw new IllegalArgumentException(InputValidMessage.REDUPLICATION.get());
+        }
+    }
+
+    public static void validDuplicate(List<Integer> number){
+        Set<Integer> lottoNo = new HashSet<>(number);
+
+        if(lottoNo.size() != number.size()){
+            throw new IllegalArgumentException(InputValidMessage.REDUPLICATION.get());
+        }
+    }
+
+
+    /**
+     * 입력받은 금액 유효성 검사
+     * @param inputMoney
+     * @return
+     */
+
     public static boolean isValidMoney(String inputMoney){
         try {
             isCheckNumber(inputMoney);
@@ -17,7 +79,7 @@ public class InputValidator {
             amoutRange(money);
             return true;
         }catch (IllegalArgumentException e) {
-            System.out.println(InputValidMessage.INPUT_MONEY_ERROR);
+            System.out.println(e.getMessage());
             return false;
         }
     }
--- src/main/java/lotto/model/constant/InputValidMessage.java
+++ src/main/java/lotto/model/constant/InputValidMessage.java
@@ -3,7 +3,8 @@ package lotto.model.constant;// @ author ninaaano
 public enum InputValidMessage {
     INPUT_MONEY_ERROR("[ERROR] 정확한 금액을 입력해주세요"),
     INPUT_NUMBER_ERROR("[ERROR] 숫자만 입력할 수 있습니다"),
-    REDUPLICATION("[ERROR] 중복된 숫자는 입력할 수 없습니다")
+    REDUPLICATION("[ERROR] 중복된 숫자는 입력할 수 없습니다"),
+    AMOUNT_RANGE("[ERROR] 1-45사이의 숫자만 입력할 수 있습니다")
     ;
 
     private final String message;
