7a9d4be55470f34c290edc04039678c6e8b76db1
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 11:45:42 +0900

    feat(DomainErrorMessage): 보너스번호 클래스 관련 에러메세지 추가

--- src/main/java/lotto/domain/constant/DomainErrorMessage.java
+++ src/main/java/lotto/domain/constant/DomainErrorMessage.java
@@ -4,6 +4,8 @@ public enum DomainErrorMessage {
     INVALID_SIZE("로또 숫자는 6개의 숫자여야 합니다."),
     INVALID_RANGE(LottoProperties.MIN_NUMBER.get() + "부터 " + LottoProperties.MAX_NUMBER.get()
             + "까지의 숫자만 허용됩니다."),
+    INVALID_RANGE_BONUS_NUMBER("보너스 번호는 " + LottoProperties.MIN_NUMBER.get() + "부터 " + LottoProperties.MAX_NUMBER.get()
+            + "까지의 숫자만 허용됩니다."),
     DUPLICATED_NUMBERS("중복된 숫자가 없도록 입력해주세요");
 
     private static final String prefix = "[ERROR] ";
