a80104b757f0f229c31cd4d33d0a2add64874887
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 11:15:15 +0900

    feat(LottoProperties): 로또 번호 관련 enum 생성

---
+++ src/main/java/lotto/domain/constant/LottoProperties.java
@@ -0,0 +1,17 @@
+package lotto.domain.constant;
+
+public enum LottoProperties {
+    SIZE(6),
+    MIN_NUMBER(1),
+    MAX_NUMBER(45);
+
+    private final int property;
+
+    LottoProperties(int property) {
+        this.property = property;
+    }
+
+    public int get() {
+        return property;
+    }
+}
