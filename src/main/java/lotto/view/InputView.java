dd32fa100581dc97336a2f3472d9fa1c9d508be0
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 14:47:40 +0900

    feat(InputView): 싱글톤 틀 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class InputView {
+    private static InputView instance = new InputView();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return instance;
+    }
+}
