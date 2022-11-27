357683b5242209fb7aaa6f60e54c2191f89dd7f6
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:53:50 +0900

    feat(LotteryController): 당첨 번호 생성 컨트롤러 기능 구현

--- src/main/java/lotto/controller/LotteryController.java
+++ src/main/java/lotto/controller/LotteryController.java
@@ -2,10 +2,12 @@ package lotto.controller;
 
 import lotto.domain.Money;
 import lotto.domain.User;
+import lotto.domain.WinningLottery;
 import lotto.service.LotteryService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class LotteryController {
@@ -28,6 +30,18 @@ public class LotteryController {
         return new User("id", money);
     }
 
+    public User purchaseTicket(User user) {
+        lotteryService.sellTickets(user);
+        outputView.printPurchasedTickets(user.createTicketsInfo());
+        return user;
+    }
+
+    public WinningLottery createWinningLottery() {
+        List<Integer> winningNumbersInput = requestNumberListInput(inputView::readWinningNumbers);
+        int bonusNumberInput = requestNumberInput(inputView::readBonusNumber);
+        return lotteryService.draw(winningNumbersInput, bonusNumberInput);
+    }
+
     private int requestNumberInput(Supplier<Integer> supplier) {
         try {
             return supplier.get();
@@ -37,9 +51,12 @@ public class LotteryController {
         }
     }
 
-    public User purchaseTicket(User user) {
-        lotteryService.sellTickets(user);
-        outputView.printPurchasedTickets(user.createTicketsInfo());
-        return user;
+    private List<Integer> requestNumberListInput(Supplier<List<Integer>> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException e){
+            outputView.printErrorMessage(e.getMessage());
+            return requestNumberListInput(supplier);
+        }
     }
 }
