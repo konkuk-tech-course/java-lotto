7295eb71370de0240e7d98ecbf77d18983c596fc
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:50:36 +0900

    feat(ViewValidator): 당첨 번호 입력 유효성 검증 구현

--- src/main/java/lotto/view/ViewValidator.java
+++ src/main/java/lotto/view/ViewValidator.java
@@ -3,6 +3,7 @@ package lotto.view;
 import lotto.view.constant.ValidationRegex;
 import lotto.view.constant.ViewErrorMessage;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class ViewValidator {
@@ -11,16 +12,34 @@ public class ViewValidator {
             throw new IllegalArgumentException(ViewErrorMessage.EMPTY.get());
         }
 
-        if (isNumber(input)) {
+        if (hasNonNumericValue(input)) {
             throw new IllegalArgumentException(ViewErrorMessage.NOT_NUMBER.get());
         }
+    }
+
+    public static void validateWinningNumberInput(List<String> numbers) {
+        if (hasEmptyValue(numbers)) {
+            throw new IllegalArgumentException(ViewErrorMessage.EMPTY.get());
+        }
+
+        if (containsNonNumericValue(numbers)) {
+            throw new IllegalArgumentException(ViewErrorMessage.NOT_NUMBER.get());
+        }
+    }
+
+    private static boolean hasEmptyValue(List<String> numbers) {
+        return numbers.stream().anyMatch(ViewValidator::isEmpty);
+    }
+
+    private static boolean containsNonNumericValue(List<String> numbers) {
+        return numbers.stream().anyMatch(ViewValidator::hasNonNumericValue);
     }
 
     private static boolean isEmpty(String input) {
         return Pattern.matches(ValidationRegex.EMPTY.get(), input);
     }
 
-    private static boolean isNumber(String input) {
-        return Pattern.matches(ValidationRegex.NUMBER.get(), input);
+    private static boolean hasNonNumericValue(String input) {
+        return !Pattern.matches(ValidationRegex.NUMBER.get(), input);
     }
 }
