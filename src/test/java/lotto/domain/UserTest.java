06b785bc28099806ae52be4d68922093047f9fd3
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 13:55:38 +0900

    test(User): 구현 기능 테스트 추가
    [세부 사항]
    - 테스트 목록
    1. 로또 구매시 1000원 차감
    2. 로또 구매시 보유 티켓 1장 증가

---
+++ src/test/java/lotto/domain/UserTest.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class UserTest {
+    private static User user;
+    private static Lotto ticket;
+
+    @BeforeEach
+    void beforeEach() {
+        user = new User("id", new Money(2000));
+        ticket = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    void When_buyTicket_Expect_MoneyDecreasedByUnit() {
+        user.buyTicket(ticket);
+        assertThat(user.getMoney()).isEqualTo(1000);
+    }
+
+    @Test
+    void When_buyTicket_Expect_hasTicket() {
+        user.buyTicket(ticket);
+        assertThat(user.getNumberOfTickets()).isEqualTo(1);
+    }
+}
