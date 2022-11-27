0af64ff7afa52991f1a2325888f8556d1e07615c
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:32:44 +0900

    feat(InputPhrase): 당첨 번호 입력 관련 문구 생성

--- src/main/java/lotto/view/constant/InputPhrase.java
+++ src/main/java/lotto/view/constant/InputPhrase.java
@@ -1,7 +1,9 @@
 package lotto.view.constant;
 
 public enum InputPhrase {
-    MONEY_INPUT("구입금액을 입력해 주세요.");
+    MONEY_INPUT("구입금액을 입력해 주세요."),
+    WINNING_NUMBER_INPUT("당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_INPUT("보너스 번호를 입력해 주세요.");
 
     private final String phrase;
 
