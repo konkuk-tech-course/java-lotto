4f61cc0e0b7db4670aaa665f2a1866b9a33998e6
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 14:56:15 +0900

    feat(ViewValidator): 숫자 입력 검증 기능 구현

---
+++ src/main/java/lotto/view/ViewValidator.java
@@ -0,0 +1,26 @@
+package lotto.view;
+
+import lotto.view.constant.ValidationRegex;
+import lotto.view.constant.ViewErrorMessage;
+
+import java.util.regex.Pattern;
+
+public class ViewValidator {
+    public static void validateNumberInput(String input) {
+        if (isEmpty(input)) {
+            throw new IllegalArgumentException(ViewErrorMessage.EMPTY.get());
+        }
+        
+        if (isNumber(input)) {
+            throw new IllegalArgumentException(ViewErrorMessage.NOT_NUMBER.get());
+        }
+    }
+
+    private static boolean isEmpty(String input) {
+        return Pattern.matches(ValidationRegex.EMPTY.get(), input);
+    }
+
+    private static boolean isNumber(String input) {
+        return Pattern.matches(ValidationRegex.NUMBER.get(), input);
+    }
+}
