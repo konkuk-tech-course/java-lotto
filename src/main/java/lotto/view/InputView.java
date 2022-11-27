2e449377536a99cfdd6eb0e397bd73ea0a117c94
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:50:54 +0900

    feat(InputView): 당첨번호 및 보너스 번호 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,14 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.view.constant.InputPhrase;
 
+import javax.swing.text.View;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
+    private static final String DELIMITER = ",";
+
     private static InputView instance = new InputView();
 
     private InputView() {
@@ -18,5 +25,31 @@ public class InputView {
         String moneyInput = Console.readLine();
         ViewValidator.validateNumberInput(moneyInput);
         return Integer.parseInt(moneyInput);
+    }
+
+    public List<Integer> readWinningNumbers() {
+        System.out.println(InputPhrase.WINNING_NUMBER_INPUT.get());
+        String winningNumberInput = Console.readLine();
+        List<String> parsedWinningNumberInput = parseWinningNumberInput(winningNumberInput);
+        ViewValidator.validateWinningNumberInput(parsedWinningNumberInput);
+        return parseToInteger(parsedWinningNumberInput);
+    }
+
+    private List<String> parseWinningNumberInput(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> parseToInteger(List<String> numbers) {
+        return numbers.stream().map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public int readBonusNumber() {
+        System.out.println(InputPhrase.BONUS_NUMBER_INPUT.get());
+        String bonusNumberInput = Console.readLine();
+        ViewValidator.validateNumberInput(bonusNumberInput);
+        return Integer.parseInt(bonusNumberInput);
     }
 }
