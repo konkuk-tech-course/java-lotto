3c13c809397d9fd1b5b8ea8b44e8f4d6cbc42caa
skydreamer21 <skydreamer210@gmail.com>
2022-11-27 15:15:57 +0900

    feat(LotteryService): 로또 구매 서비스 기능 구현

--- src/main/java/lotto/service/LotteryService.java
+++ src/main/java/lotto/service/LotteryService.java
@@ -1,6 +1,8 @@
 package lotto.service;
 
 import lotto.controller.LotteryController;
+import lotto.domain.Lotto;
+import lotto.domain.User;
 
 public class LotteryService {
     private static LotteryService instance = new LotteryService();
@@ -11,5 +13,16 @@ public class LotteryService {
     public static LotteryService getInstance() {
         return instance;
     }
-    
+
+    public User sellTickets(User user) {
+        while (user.hasMoney()) {
+            Lotto ticket = createTicket();
+            user.buyTicket(ticket);
+        }
+        return user;
+    }
+
+    private Lotto createTicket() {
+        return new Lotto(LottoNumberGenerator.generate())
+    }
 }
