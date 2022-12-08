bc1baab3dd23cb688fb947695ad04e008b9151fe
juno-junho <ssmm0205@naver.com>
2022-12-08 17:48:40 +0900

    feat(InputView): 구입금액을 입력받는 메서드 생성
    validate 메서드로 검증

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,22 @@
 package view;
 
+import camp.nextstep.edu.missionutils.Console;
+import utils.Util;
+
 public class InputView {
+    public static final int UNIT = 1_000;
+
+    public int readMoney() {
+        int money = Util.convertStringToInt(Console.readLine());
+        validateMoney(money);
+        return money;
+    }
+
+    private void validateMoney(int money) {
+        checkUnit(money % UNIT);
+    }
+
+    private void checkUnit(int remainder) {
+        if (remainder != 0 ) throw new IllegalArgumentException("[ERROR] 1,000 단위로 입력해야 합니다.");
+    }
 }
