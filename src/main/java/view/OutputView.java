d91dc32cb8b38f1113e574e9369038e87c42442d
juno-junho <ssmm0205@naver.com>
2022-12-08 20:04:30 +0900

    feat(OutputView): 생성한 로또를 출력하는 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,17 @@
 package view;
 
+import domain.LottoGenerator;
+import lotto.Lotto;
+
+import java.util.List;
+
 public class OutputView {
+
+    public void printLottery(int lottoCount) {
+        System.out.println(lottoCount + "개를 구매했습니다.");
+        LottoGenerator lottoGenerator = new LottoGenerator(lottoCount);
+        List<Lotto> lottos = lottoGenerator.generateLotto();
+        lottos.forEach(System.out::println);
+
+    }
 }
