e01da18d28b362d59fee3ea66b154adc93833a11
juno-junho <ssmm0205@naver.com>
2022-12-08 20:00:04 +0900

    feat(Lotto): 로또를 오름 차순 정렬하는 메서드 성
    sortNumber를 통해 로또 객체 생성시 자동 오름차순 정렬 설정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,33 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
+    private static final String DELIMITER = ", ";
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        sortNumber(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또의 크기가 6이 아닙니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    // lotto에서 sort하는 기능
+    private void sortNumber(List<Integer> numbers) {
+        Collections.sort(numbers, Comparator.naturalOrder());
+    }
+
 }
