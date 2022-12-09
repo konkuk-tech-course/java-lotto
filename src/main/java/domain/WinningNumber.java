cae80aec9f629f27aca51fba9196aabedfcd810e
juno-junho <ssmm0205@naver.com>
2022-12-09 12:48:33 +0900

    refactor(WinningNumber): 타입 Lotto로 변환

--- src/main/java/domain/WinningNumber.java
+++ src/main/java/domain/WinningNumber.java
@@ -1,18 +1,24 @@
 package domain;
 
+import lotto.Lotto;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class WinningNumber {
 
     private final List<Integer> winningNumbers;
+    private int bonusNumber;
 
-    public List<Integer> getWinningNumbers() {
-        return winningNumbers;
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningNumbers() {
+        return new Lotto(winningNumbers);
     }
 
     public WinningNumber() {
         winningNumbers = new ArrayList<>();
-
     }
 }
