b1910d7905bd9c1fd43d6a6901a8ae4a54cefb28
juno-junho <ssmm0205@naver.com>
2022-12-09 18:42:21 +0900

    feat(Rank): count로 enum을 찾는 메서드 추가

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,20 +1,30 @@
 package domain;
 
-public enum Rank {
+import java.util.Arrays;
+
+public enum
+Rank {
     FIRST(6, 2_000_000_000),
     SECOND(5, 30_000_000),
-    THRID(5, 1_500_000),
+    THIRD(5, 1_500_000),
     FOURTH(4, 50_000),
     FIFTH(3, 5_000);
 
     private final int winningCount;
     private final int winningMoney;
 
+
     Rank(int winningCount, int winningMoney) {
         this.winningCount = winningCount;
         this.winningMoney = winningMoney;
     }
 
+    public static Rank findByMatchCount(int matchedCount) {
+        return Arrays.stream(Rank.values())
+                .filter(i -> i.getWinningCount() == matchedCount)
+                .findAny()
+                .get();
+    }
     public int getWinningCount() {
         return winningCount;
     }
